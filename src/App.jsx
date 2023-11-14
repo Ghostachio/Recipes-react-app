a6f67b9280aa4f7ca6eba2a58406ab3284191b1b
Ghostachio <mansour.hamza2.0@gmail.com>
2023-11-14 21:11:21 +0100

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import Form from './components/Form';
 import Recipes from './components/Recipes';
-import Title from './components/title';
+import Title from './components/Title';
 
 function App() {
   
